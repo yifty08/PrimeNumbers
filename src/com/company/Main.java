61d2aed81dd6818281424dfadf198e78aa1fea20
Yifty <yifty08@icloud.com>
2019-04-19 14:11:03 -0400

    Enter a number and see the magic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 50; i++) {
+        Scanner scanner = new Scanner(System.in);
 
-            if (i==1){
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        for (int i=1; i <= num; i++){
+            if (i == 1) {
                 System.out.println(i + " is not a prime number.");
-            } else if (i/1==i && i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0){
+            } else if (i / 1 == i && i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
                 System.out.println(i + " is a prime number.");
-            } else if (i==2 || i==3 || i==5 || i==7){
+            } else if (i == 2 || i == 3 || i == 5 || i == 7) {
                 System.out.println(i + " is a prime number.");
             } else {
                 System.out.println(i + " is not a prime number.");
