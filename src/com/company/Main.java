594e9345a1737a4c4cfeb8a316feff02d7962e3b
Yifty <yifty08@icloud.com>
2019-04-19 13:56:07 -0400

    Prime numbners 1-50

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world;]");
+
+        for (int i = 1; i <= 50; i++) {
+
+            if (i==1){
+                System.out.println(i + " is not a prime number.");
+            } else if (i/1==i && i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0){
+                System.out.println(i + " is a prime number.");
+            } else if (i==2 || i==3 || i==5 || i==7){
+                System.out.println(i + " is a prime number.");
+            } else {
+                System.out.println(i + " is not a prime number.");
+            }
+        }
     }
 }
